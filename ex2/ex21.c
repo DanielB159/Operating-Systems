942985895fb4d7c2ceb7e9d11976cd435234560c
Daniel Boianju <daniel.b159357@gmail.com>
2023-04-24 13:56:50 +0300

    identical finished

--- ex2/ex21.c
+++ ex2/ex21.c
@@ -8,6 +8,44 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+typedef struct HashItem{
+    char key;
+    int numTimes;
+    struct HashItem *next;
+} HashItem;
+
+// this function checks weather the files described by fd1 and fd2 are identical
+int areIdentical(int fd1, int fd2) {
+    char file1read[1], file2read[1];
+    //read the first byre of the file.
+    int readOut1 = read(fd1, file1read, 1), readOut2 = read(fd2, file2read, 1);
+    // while at least one of the files is still being read
+    while (readOut1 || readOut2) {
+        // check if the reading has failed for one of the files
+        if (readOut1 == -1 || readOut2 == -1) {
+            if (write(1, "Error in: access\n", 18) == -1) {
+                exit(-1);
+            }
+            exit(-1);
+        }
+        // next, check if one of the files ended before the other
+        if (readOut1 && !readOut2) {
+            return 0;
+        }
+        if (!readOut1 && readOut2) {
+            return 0;
+        }
+        // next, check if the read byte is equal
+        if (file1read[0] != file2read[0]) {
+            return 0;
+        }
+        // keep reading
+        readOut1 = read(fd1, file1read, 1);
+        readOut2 = read(fd2, file2read, 1);
+    }
+    // if passed all tests, the files are indeed equal
+    return 1;
+}
 
 
 int main(int argc, char *argv[]) {
@@ -31,8 +69,30 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
+    // first, check if the files are identical
+    if (areIdentical(fd1, fd2)) {
+        return 1;
+    }
+    // DANIEL IMPLEMENT RE READING USING SEEK OR SOMETGHING
+
+    // opening the files for a second reading
+    fd1 = open(argv[1], O_RDONLY), fd2 = open(argv[2], O_RDONLY);
+    if (fd1 == -1 || fd2 == -1) {
+        if (write(1, "Error in: open\n", 16) == -1) {
+            exit(-1);
+        }
+        exit(-1);
+    }
+
+    char file1read[1], file2read[1];
+    //read the first byre of the file.
+    while (read(fd1, file1read, 1) && read(fd2, file2read, 1)) {
+        printf("%c", file1read[0]);
+        printf("%c", file2read[0]);
+    }
+
 
     // closing the opened files
     close(fd1);
-    close(fd2);
+    close(fd2);   
 }
